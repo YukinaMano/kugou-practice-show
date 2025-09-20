06888b2fc371b96c4e7f8032a768ad744f1fb130
AlsoCode <alsoeasy0409@163.com>
2025-09-20 19:29:47 +0800

    更新-接入网络请求

--- pages/login.vue
+++ pages/login.vue
@@ -47,6 +47,7 @@
 <script setup>
 import { ref } from 'vue'
 import { debugLog } from '/utils/debug'
+import { api } from '@/api'
 
 const ifShowInput = ref(false)
 const ifShowSignUp = ref(false)
@@ -56,7 +57,9 @@ const pass = ref("123")
 // 登录逻辑
 const _login = async () => {
   debugLog(acc.value, pass.value)
-  uni.redirectTo({ url: '/pages/index/index' })
+  const res = await api.user.login(acc.value, pass.value)
+  debugLog(res)
+  // uni.redirectTo({ url: '/pages/index/index' })
   // uni.switchTab({ url: '/pages/index/index' }) // @YHD#mk>更新为tarBar后替换
 }
 
