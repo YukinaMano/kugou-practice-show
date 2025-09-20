0360fc582ffb1712b7e279e7c2cab74ffd5bfba4
AlsoCode <alsoeasy0409@163.com>
2025-09-21 07:55:58 +0800

    修改-二进制资源索引方式

--- mock/showdata.js
+++ mock/showdata.js
@@ -1,7 +1,7 @@
 import { makeHashKey } from "@/utils/common.js";
 
-const key1 = makeHashKey({ username: 'test', password: '123' })
-const key2 = makeHashKey({ username: 'scholar', password: '456' })
+const key1 = makeHashKey({ username: 'scholar', password: '456' })
+const key2 = makeHashKey({ username: 'test', password: '123' })
 
 export const users = {
   [key1]: {
@@ -14,61 +14,63 @@ export const users = {
     id: 2,
     username: 'scholar',
     password: '456',
-    toekn: key2
+    token: key2
   }
 };
 
-const basePath = '/mock/assest/'
+const basePath = '/mock/assets/'
+
 export const musicList = [
   {
     mId: 1,
     mTitle: '鱼玄机',
     mSinger: 'hanser',
-    mPictureUrl: `${basePath}1.jpg`,
-    mMusicUrl: `${basePath}hanser - 鱼玄机.ogg`,
-    mLyricUrl: `${basePath}hanser - 鱼玄机.lrc`
+    mPictureUrl: new URL(basePath + '1.jpg', import.meta.url).href,
+    mMusicUrl: new URL(basePath + 'hanser - 鱼玄机.ogg', import.meta.url).href,
+    mLyricUrl: new URL(basePath + 'hanser - 鱼玄机.lrc', import.meta.url).href
   },
   {
     mId: 2,
     mTitle: 'アイシテ (爱我吧)',
     mSinger: 'とあ (toa)',
-    mPictureUrl: `${basePath}2.png`,
-    mMusicUrl: `${basePath}とあ (toa) _ nameless - アイシテ (爱我吧).ogg`,
-    mLyricUrl: `${basePath}とあ (toa) _ nameless - アイシテ (爱我吧).lrc`
+    mPictureUrl: new URL(basePath + '2.png', import.meta.url).href,
+    mMusicUrl: new URL(basePath + 'とあ (toa) _ nameless - アイシテ (爱我吧).ogg', import.meta.url).href,
+    mLyricUrl: new URL(basePath + 'とあ (toa) _ nameless - アイシテ (爱我吧).lrc', import.meta.url).href
   },
   {
     mId: 3,
-    mTitle: '心拍数#0822',
-    mSinger: '鹿乃 (かの)',
-    mPictureUrl: `${basePath}3.jpg`,
-    mMusicUrl: `${basePath}鹿乃 (かの) - 心拍数#0822.ogg`,
-    mLyricUrl: `${basePath}鹿乃 (かの) - 心拍数#0822.lrc`
+    mTitle: '打上花火',
+    mSinger: 'Daoko (ダヲコ)',
+    mPictureUrl: new URL(basePath + '3.jpg', import.meta.url).href,
+    mMusicUrl: new URL(basePath + 'Daoko (ダヲコ) _ 米津玄師 (よねづ けんし) - 打上花火.ogg', import.meta.url).href,
+    mLyricUrl: new URL(basePath + 'Daoko (ダヲコ) _ 米津玄師 (よねづ けんし) - 打上花火.lrc', import.meta.url).href
   },
   {
     mId: 4,
     mTitle: '少年诗',
     mSinger: '米白',
-    mPictureUrl: `${basePath}4.jpg`,
-    mMusicUrl: `${basePath}米白 - 少年诗.ogg`,
-    mLyricUrl: `${basePath}米白 - 少年诗.lrc`
+    mPictureUrl: new URL(basePath + '4.jpg', import.meta.url).href,
+    mMusicUrl: new URL(basePath + '米白 - 少年诗.ogg', import.meta.url).href,
+    mLyricUrl: new URL(basePath + '米白 - 少年诗.lrc', import.meta.url).href
   },
   {
     mId: 5,
     mTitle: '乐园游梦记',
     mSinger: '耀佳音',
-    mPictureUrl: `${basePath}5.jpg`,
-    mMusicUrl: `${basePath}三Z-STUDIO _ HOYO-MiX - 乐园游梦记.flac`,
-    mLyricUrl: `${basePath}三Z-STUDIO _ HOYO-MiX - 乐园游梦记.lrc`
+    mPictureUrl: new URL(basePath + '5.jpg', import.meta.url).href,
+    mMusicUrl: new URL(basePath + '三Z-STUDIO _ HOYO-MiX - 乐园游梦记.flac', import.meta.url).href,
+    mLyricUrl: new URL(basePath + '三Z-STUDIO _ HOYO-MiX - 乐园游梦记.lrc', import.meta.url).href
   },
   {
     mId: 6,
     mTitle: '在银河中孤独摇摆',
     mSinger: '知更鸟',
-    mPictureUrl: `${basePath}6.jpg`,
-    mMusicUrl: `${basePath}知更鸟 _ HOYO-MiX _ Chevy - 在银河中孤独摇摆.ogg`,
-    mLyricUrl: `${basePath}知更鸟 _ HOYO-MiX _ Chevy - 在银河中孤独摇摆.lrc`
+    mPictureUrl: new URL(basePath + '6.jpg', import.meta.url).href,
+    mMusicUrl: new URL(basePath + '知更鸟 _ HOYO-MiX _ Chevy - 在银河中孤独摇摆.ogg', import.meta.url).href,
+    mLyricUrl: new URL(basePath + '知更鸟 _ HOYO-MiX _ Chevy - 在银河中孤独摇摆.lrc', import.meta.url).href
   }
 ]
+
 
 
 export const playlist = {
