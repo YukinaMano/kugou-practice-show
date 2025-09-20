f397688e1a35ca6a43d002f645c8dad1c1cf5399
AlsoCode <alsoeasy0409@163.com>
2025-09-21 06:15:57 +0800

    提取静态资源路径

--- mock/showdata.js
+++ mock/showdata.js
@@ -18,50 +18,58 @@ export const users = {
   }
 };
 
+const basePath = '/mock/assest/'
 export const musicList = [
   {
-  mId: 1,
-  mTitle: '鱼玄机',
-  mPictureUrl: '@/mock/assest/1.jpg',
-  mMusicUrl: '@/mock/assest/hanser - 鱼玄机.ogg',
-  mLyricUrl: '@/mock/assest/hanser - 鱼玄机.lrc'
+    mId: 1,
+    mTitle: '鱼玄机',
+    mSinger: 'hanser',
+    mPictureUrl: `${basePath}1.jpg`,
+    mMusicUrl: `${basePath}hanser - 鱼玄机.ogg`,
+    mLyricUrl: `${basePath}hanser - 鱼玄机.lrc`
   },
   {
-  mId: 2,
-  mTitle: 'アイシテ (爱我吧)',
-  mPictureUrl: '@/mock/assest/2.png',
-  mMusicUrl: '@/mock/assest/とあ (toa) _ nameless - アイシテ (爱我吧).ogg',
-  mLyricUrl: '@/mock/assest/とあ (toa) _ nameless - アイシテ (爱我吧).lrc'
+    mId: 2,
+    mTitle: 'アイシテ (爱我吧)',
+    mSinger: 'とあ (toa)',
+    mPictureUrl: `${basePath}2.png`,
+    mMusicUrl: `${basePath}とあ (toa) _ nameless - アイシテ (爱我吧).ogg`,
+    mLyricUrl: `${basePath}とあ (toa) _ nameless - アイシテ (爱我吧).lrc`
   },
   {
-  mId: 3,
-  mTitle: '心拍数#0822',
-  mPictureUrl: '@/mock/assest/3.jpg',
-  mMusicUrl: '@/mock/assest/鹿乃 (かの) - 心拍数#0822.ogg',
-  mLyricUrl: '@/mock/assest/鹿乃 (かの) - 心拍数#0822.lrc'
+    mId: 3,
+    mTitle: '心拍数#0822',
+    mSinger: '鹿乃 (かの)',
+    mPictureUrl: `${basePath}3.jpg`,
+    mMusicUrl: `${basePath}鹿乃 (かの) - 心拍数#0822.ogg`,
+    mLyricUrl: `${basePath}鹿乃 (かの) - 心拍数#0822.lrc`
   },
   {
-  mId: 4,
-  mTitle: '少年诗',
-  mPictureUrl: '@/mock/assest/4.jpg',
-  mMusicUrl: '@/mock/assest/米白 - 少年诗.ogg',
-  mLyricUrl: '@/mock/assest/米白 - 少年诗.lrc'
+    mId: 4,
+    mTitle: '少年诗',
+    mSinger: '米白',
+    mPictureUrl: `${basePath}4.jpg`,
+    mMusicUrl: `${basePath}米白 - 少年诗.ogg`,
+    mLyricUrl: `${basePath}米白 - 少年诗.lrc`
   },
   {
-  mId: 5,
-  mTitle: '乐园游梦记',
-  mPictureUrl: '@/mock/assest/5.jpg',
-  mMusicUrl: '@/mock/assest/三Z-STUDIO _ HOYO-MiX - 乐园游梦记.flac',
-  mLyricUrl: '@/mock/assest/三Z-STUDIO _ HOYO-MiX - 乐园游梦记.lrc'
+    mId: 5,
+    mTitle: '乐园游梦记',
+    mSinger: '耀佳音',
+    mPictureUrl: `${basePath}5.jpg`,
+    mMusicUrl: `${basePath}三Z-STUDIO _ HOYO-MiX - 乐园游梦记.flac`,
+    mLyricUrl: `${basePath}三Z-STUDIO _ HOYO-MiX - 乐园游梦记.lrc`
   },
   {
-  mId: 6,
-  mTitle: '在银河中孤独摇摆',
-  mPictureUrl: '@/mock/assest/6.jpg',
-  mMusicUrl: '@/mock/assest/知更鸟 _ HOYO-MiX _ Chevy - 在银河中孤独摇摆.ogg',
-  mLyricUrl: '@/mock/assest/知更鸟 _ HOYO-MiX _ Chevy - 在银河中孤独摇摆.lrc'
+    mId: 6,
+    mTitle: '在银河中孤独摇摆',
+    mSinger: '知更鸟',
+    mPictureUrl: `${basePath}6.jpg`,
+    mMusicUrl: `${basePath}知更鸟 _ HOYO-MiX _ Chevy - 在银河中孤独摇摆.ogg`,
+    mLyricUrl: `${basePath}知更鸟 _ HOYO-MiX _ Chevy - 在银河中孤独摇摆.lrc`
   }
 ]
+
 
 export const playlist = {
   [key1]: [
